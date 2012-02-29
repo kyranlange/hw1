f1973a3bb6ed4613160654791e155d7d8542b2a7
Kyran Lange <kyran.lange@gmail.com>
2012-02-29 03:27:50 -0800

    Finished part 5

--- part5.rb
+++ part5.rb
@@ -6,9 +6,10 @@ class Class
     class_eval %Q{def #{attr_name}=(val)
       if @#{attr_name}_history.nil? 
         @#{attr_name}_history = Array.new
+        @#{attr_name}_history << @#{attr_name}
       end
-      @#{attr_name}_history << @#{attr_name}
       @#{attr_name} = val; 
+      @#{attr_name}_history << @#{attr_name}
     end}
   end
 end
